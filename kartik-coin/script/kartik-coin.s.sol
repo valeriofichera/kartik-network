45276518c22758046ac515b498e1770cc5ae9225
Valerio <78494380+valeriofichera@users.noreply.github.com>
2024-03-17 01:59:14 +0000

    add

--- kartik-coin/script/kartik-coin.s.sol
+++ kartik-coin/script/kartik-coin.s.sol
@@ -1,17 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
 import {Script, console} from "forge-std/Script.sol";
 import {KartikCoin} from "../src/kartik-coin.sol";
+import {QuizContract} from "../src/quiz-contract.sol";
 
-contract Deploy is Script {
+contract DeployKartikCoin is Script {
     function setUp() public {}
+
+    address DAO = 0x023FD79587ca4c1dfd8512b5d374e685D831fa59;
 
     function run() public {
         vm.startBroadcast();
         KartikCoin kartikCoin = new KartikCoin(msg.sender);
         console.log("Kartik Coin deployed at:", address(kartikCoin));
+        kartikCoin.transfer(DAO, 69420000000 * 10 ** 18);
+        console.log("Kartik Coin send to DAO:", address(DAO));
 
+        QuizContract quizContract = new QuizContract(msg.sender);
+        console.log("Quiz Contract deployed at:", address(quizContract));
         // quiz contract = new quizcontract()
         // kartikCoin.transfer(total.supply)
         vm.stopBroadcast();
