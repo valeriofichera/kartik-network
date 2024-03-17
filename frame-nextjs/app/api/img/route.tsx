f6a2c834f59dda8ace370741c00ee7f8550fee94
Merlin <50379729+AsparAugustus@users.noreply.github.com>
2024-03-17 09:03:10 +0000

    max width

--- frame-nextjs/app/api/img/route.tsx
+++ frame-nextjs/app/api/img/route.tsx
@@ -17,28 +17,20 @@ export async function GET(req: NextRequest) {
 
   return new ImageResponse(
   (
-      <Card>
-    <h1
-      style={{
-        color,
-        fontSize: 40, // Set the initial font size
-        wordWrap: 'break-word', // Enable word wrapping
-        maxWidth: '100%', // Ensure the text doesn't exceed the container width
-      }}
-    >
-      {questionString}
-    </h1>
-    <style jsx>{`
-      h1 {
-        max-height: 200px; // Example maximum height for the text
-        overflow: hidden; // Hide overflow text
-        text-overflow: ellipsis; // Show ellipsis (...) for overflow text
-      }
-    `}</style>
-    <div style={{ display: "flex", marginTop: "12" }}>
-      {/* Other content */}
-    </div>
-  </Card>
+    <Card>
+      <h1
+        style={{
+          color,
+          fontSize: 40,
+          wordWrap: 'break-word', // or 'overflow-wrap: break-word;'
+          maxWidth: '60%', // or any other suitable value
+        }}
+      >
+        {questionString}
+      </h1>
+      <div style={{ display: "flex", marginTop: "12" }}>
+      </div>
+    </Card>
   ),
   {
     width: 1600,
