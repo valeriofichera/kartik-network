f44599dd640745f60263b2125638a27ea3b7874e
Merlin <50379729+AsparAugustus@users.noreply.github.com>
2024-03-17 02:30:00 +0000

    changed api route

--- frame-nextjs/app/config.ts
+++ frame-nextjs/app/config.ts
@@ -1,3 +1,3 @@
 export const NEXT_PUBLIC_URL = process.env.NODE_ENV === 'development'
   ? 'http://localhost:3000'
-  : 'https://quiz-hackathon-frame.vercel.app';
+  : 'https://kartik-network.vercel.app/';
